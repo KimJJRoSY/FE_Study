a2214ae068cbb6b262dfa2661f9a2d4700978cfd
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-08-28 12:17:37 +0900

    setting: 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import WritePage from "./pages/write";
+import styled from "styled-components";
+
 
 function App() {
   return (
-    <>
-    </>
+    <Wrapper>
+      <Routes>
+          <Route path="/write" element={<WritePage/>} />
+      </Routes>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+    overflow-y: scroll;
+    //-ms-overflow-style: none;
+    //scrollbar-width: none;
+`
